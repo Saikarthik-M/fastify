45bdfe6c3a78782ceeed38a34bfcd65ac46f9a4f
Sai Karthik M <saikarthikofficially@gmail.com>
2021-05-21 12:18:48 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ routes.forEach((routes) => {
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(process.env.PORT | 3000)
+        await fastify.listen(process.env.PORT || 3000)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
